9df02a60ee9b886d33c7deeb5761ad2c58d30004
Rishabh Negi <negirishabh7011@gmail.com>
2025-10-07 14:45:30 +0530

    created models and connected to DB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 8080;
@@ -8,62 +9,46 @@ const PORT = 8080;
 app.use(cors());
 app.use(express.json());
 
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    connectDB()
 })
+const connectDB = async()=>{
+    try{
+        await mongoose.connect(process.env.MONGOBD_URI);
+        console.log("connected with Database")
 
-app.post("/test", async (req, res) => {
-    const options = {
-        method: 'POST',
-        headers: {
-            "content-type": "application/json",
-            "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
-        },
-        body: JSON.stringify({
-            model: 'openai/gpt-4o',
-            messages: [
-                {
-                    role: 'user',
-                    content: req.body.message,
-                },
-            ],
-        })
+    }catch(err){
+        console.log(`Failed to connect with DB ${err}`);
     }
-    try {
-        const response = await fetch("https://openrouter.ai/api/v1/chat/completions", options);
-        const data = await response.json();
-        // console.log(data.choices[0].message.content);
-        res.send(data.choices[0].message.content);
-    } catch (err) {
-        console.error(err);
-    }
-});
-fetch("https://openrouter.ai/api/v1")
+}
 
-// import OpenAI from 'openai';
-// import 'dotenv/config';
-
-// const openai = new OpenAI({
-//   baseURL: 'https://openrouter.ai/api/v1',
-//   apiKey: process.env.OPENAI_API_KEY,
-//   defaultHeaders: {
-//     'HTTP-Referer': '<YOUR_SITE_URL>', // Optional. Site URL for rankings on openrouter.ai.
-//     'X-Title': '<YOUR_SITE_NAME>', // Optional. Site title for rankings on openrouter.ai.
-//   },
+// app.post("/test", async (req, res) => {
+//     const options = {
+//         method: 'POST',
+//         headers: {
+//             "content-type": "application/json",
+//             "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+//         },
+//         body: JSON.stringify({
+//             model: 'openai/gpt-4o',
+//             messages: [
+//                 {
+//                     role: 'user',
+//                     content: req.body.message,
+//                 },
+//             ],
+//         })
+//     }
+//     try {
+//         const response = await fetch("https://openrouter.ai/api/v1/chat/completions", options);
+//         const data = await response.json();
+//         // console.log(data.choices[0].message.content);
+//         res.send(data.choices[0].message.content);
+//     } catch (err) {
+//         console.error(err);
+//     }
 // });
 
-// async function main() {
-//   const completion = await openai.chat.completions.create({
-//     model: 'openai/gpt-4o',
-//     messages: [
-//       {
-//         role: 'user',
-//         content: '',
-//       },
-//     ],
-//   });
 
-//   console.log(completion.choices[0].message);
-// }
-
-// main();
---
+++ backend/utils/openai.js
@@ -0,0 +1,30 @@
+import "dotenv/config";
+
+const getOpenAIResponse = async(message)=>{
+        const options = {
+        method: 'POST',
+        headers: {
+            "content-type": "application/json",
+            "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+        },
+        body: JSON.stringify({
+            model: 'openai/gpt-4o',
+            messages: [
+                {
+                    role: 'user',
+                    content: req.body.message,
+                },
+            ],
+        })
+    }
+    try {
+        const response = await fetch("https://openrouter.ai/api/v1/chat/completions", options);
+        const data = await response.json();
+        // console.log(data.choices[0].message.content);
+        return data.choices[0].message.content
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export default getOpenAIResponse;
