0cdf4fa9db0926376cf97ffc3290a7a8ad27528a
Rishabh Negi <negirishabh7011@gmail.com>
2025-10-08 16:00:00 +0530

    structure to Slidebar

--- frontend/src/Sidebar/Sidebar.jsx
+++ frontend/src/Sidebar/Sidebar.jsx
@@ -2,14 +2,27 @@ import "./Sidebar.css";
 function Sidebar() {
     return ( 
         <>
-        <div>
+        <section className="sidebar">
            {/* new chat Button */}
-           
+           <button>
+            <img className="logo"  src="https://imgs.search.brave.com/OC4-UKgf1zeU4NZ8XcZs8i0AEfcUMfY8dTp4jnF5dMk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jaGF0/Z3B0YWlodWIuY29t/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDIz/LzA2L0NoYXRHcHQt/TG9nby13aXRoLUJs/YWNrLUJhY2tncm91/bmQucG5n"></img>
+            <span><i className="fa-solid fa-pen-to-square"></i></span>
+           </button>
 
            {/* history */}
+           <ul className="history">
+            <li>history1</li>
+            <li>history1</li>
+            <li>history1</li>
+            <li>history1</li>
+           </ul>
 
-           {/* Signin */}
-        </div>
+           {/* {Created by} */}
+           <div className="created-by">
+            <p>by Rishabh Negi</p>
+            <p>Contact:<a href="https://www.linkedin.com/in/rishabh-negi-877360286/">linkedin</a></p>
+           </div>
+        </section>
         </>
      );
 }
