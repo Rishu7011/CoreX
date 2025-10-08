576688b77794029769b45172fd42b38191ba9011
Rishabh Negi <negirishabh7011@gmail.com>
2025-10-08 18:45:10 +0530

    added basic css on chatwindow

--- frontend/src/ChatWindow/ChatWindow.jsx
+++ frontend/src/ChatWindow/ChatWindow.jsx
@@ -1,9 +1,24 @@
 import "./ChatWindow.css"
+import Chat from "../Chat/Chat.jsx"
+
 function ChatWindow() {
     return ( 
         <>
-        <div>
-            Chatwindow is here
+        <div className="chatWindow">
+            <div className="navbar">
+                <span >BYTEBUDDY  <i className="fa-solid fa-chevron-down"></i></span>
+                <div className="userIconDiv"><span ><i className="userIcon fa-solid fa-circle-user"></i></span></div>
+            </div>
+            <Chat></Chat>
+            <div className="chatInput">
+                <div className="inputBox">
+                    <input placeholder="Ask anything"></input>
+                    <div id="submit"><i class="fa-solid fa-paper-plane"></i></div>
+                </div>
+                <p className="info">
+                    BYTEBUDDY can make mistakes. check important info. See cookie Preferences.
+                </p>
+            </div>
         </div>
         </>
      );
