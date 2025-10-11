bba94de35bb244bf8e930e8f2dc04771703a6842
Rishabh Negi <negirishabh7011@gmail.com>
2025-10-11 13:00:00 +0530

    add TypingIndicator for streaming responses

---
+++ frontend/src/components/TypingIndicator.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const TypingIndicator = () => (
+  <div className="typing-indicator" aria-label="CoreX is typing"><span /><span /><span /></div>
+);
+export default TypingIndicator;
