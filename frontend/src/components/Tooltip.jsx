c4cf9545870994941be7c58bb119401749def5fb
Rishabh Negi <negirishabh7011@gmail.com>
2025-10-11 09:00:00 +0530

    add Tooltip component

---
+++ frontend/src/components/Tooltip.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+const Tooltip = ({ text, children }) => {
+  const [show, setShow] = useState(false);
+  return (
+    <div className="tooltip-wrapper" onMouseEnter={() => setShow(true)} onMouseLeave={() => setShow(false)}>
+      {children}
+      {show && <div className="tooltip-box">{text}</div>}
+    </div>
+  );
+};
+export default Tooltip;
