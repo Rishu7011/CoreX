49b261261b9c79754299563a800f7af9ce275813
Rishabh Negi <negirishabh7011@gmail.com>
2025-10-11 15:00:00 +0530

    add lightweight markdown parser

---
+++ frontend/src/utils/markdownParser.js
@@ -0,0 +1,7 @@
+export const parseMarkdown = (text) =>
+  text
+    .replace(/```([\s\S]*?)```/g, "<pre><code>$1</code></pre>")
+    .replace(/`([^`]+)`/g, "<code>$1</code>")
+    .replace(/\*\*(.+?)\*\*/g, "<strong>$1</strong>")
+    .replace(/\*(.+?)\*/g, "<em>$1</em>")
+    .replace(/\n/g, "<br/>");
