3d12f732d333cad1718112f5bc61369855e6d647
Rishabh Negi <negirishabh7011@gmail.com>
2025-10-21 12:35:00 +0530

    add User Management Table component

---
+++ frontend/src/admin/UserTable.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+export const UserTable = ({ users }) => (
+  <table className="user-table">
+    <thead><tr><th>ID</th><th>Name</th><th>Role</th></tr></thead>
+  </table>
+);
